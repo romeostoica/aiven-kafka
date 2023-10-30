445c658cd903c51d618f8828dcc2cc4e5267e0fb
romeostoica <98601406+romeostoica@users.noreply.github.com>
2023-10-30 16:29:38 +0000

    Add files via upload

---
+++ kafka-aiven.py
@@ -0,0 +1,48 @@
+#from kafka import KafkaProducer
+from confluent_kafka import Producer
+import json
+import uuid
+from datetime import datetime
+from faker import Faker
+
+# Define the Kafka broker and topic details
+bootstrap_servers = "kafka-275297e7-romeo-f2fe.a.aivencloud.com:21359"
+topic = "iottopic"
+
+# Create a Kafka producer instance
+producer_config = {
+    'bootstrap.servers':bootstrap_servers,
+    'security.protocol':'SSL',
+    'ssl.ca.location':'ca.pem',
+    'ssl.certificate.location':'service.cert',
+    'ssl.key.location':'service.key'
+}
+
+producer = Producer(producer_config)
+
+# Function to generate a random event in JSON format
+def generate_random_event():
+    fake = Faker()
+    event_id = fake.uuid4()
+    timestamp = fake.date_time_this_decade(tzinfo=None).isoformat()
+    temperature = fake.random_int(min=0, max=40)
+    humidity = fake.random_int(min=30, max=100)
+    event_data = {
+        "event_id": event_id,
+        "timestamp": timestamp,
+	"temperature": temperature,
+	"humidity": humidity
+    }
+    return json.dumps(event_data)
+
+# Produce JSON data to the Kafka topic
+fake = Faker()
+event_data = generate_random_event()
+producer.produce(topic, key=str(fake.uuid4()), value=event_data)
+print(f"Produced message: {event_data}")
+
+producer.flush()
+
+# Close the producer when done
+# producer.close()
+
